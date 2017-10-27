e021b7170ffebe08b36be34d0ab34b5f0605d6c5
David Zou <davidzsy20@Davids-MacBook-Pro.local>
2017-10-27 17:06:52 -0400

    changes

--- hacker-arena/src/Components/About.js
+++ hacker-arena/src/Components/About.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import video from "video.js"
+// import video from "video.js"
 import '../Styles/About.css';
 const About = (props) => {  
   return (
-  <div>
+  <div className='overlay'>
     
     <h2>This is a game for coders to compete against each other!</h2>
-    <video className ="video" autoPlay muted loop>
+    <div className = 'banner'>
+    <video className ="banner__video" id ='Code' autoPlay muted loop>
     <source src="https://www.dropbox.com/s/nzk09kfbcxv9cqq/We-Work-We-Wait.mp4?raw=1" type="video/mp4"/>
     </video>
-  </div>
+    </div>
+    <div>
+ adfafd
+      </div>
+      </div>
 )};
 
 export default About;
